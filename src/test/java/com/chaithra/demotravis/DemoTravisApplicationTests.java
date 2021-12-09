afb13475eb2461a2a951d97d2fdbc4754a0c0e01
chaithra-ka <95607823+chaithra-ka@users.noreply.github.com>
2021-12-09 11:26:07 +0530

    Add files via upload

---
+++ src/test/java/com/chaithra/demotravis/DemoTravisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaithra.demotravis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTravisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
